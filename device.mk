a1c2128354f72bae85f313e15f690fda2d90a6f9
bengris32 <bengris32@protonmail.ch>
2023-09-05 13:05:53 +0100

    ruby: Build `rebalance_interrupts` service
    Change-Id: I193820ee14956bf227eab957017c58e8bdd86831
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -301,6 +301,9 @@ PRODUCT_PACKAGES += \
     vendor.mediatek.hardware.mtkpower@1.1.vendor \
     vendor.mediatek.hardware.mtkpower@1.2.vendor
 
+PRODUCT_PACKAGES += \
+    rebalance_interrupts-vendor.mediatek
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
